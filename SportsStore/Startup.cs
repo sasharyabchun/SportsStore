e075eb6fe94f5bc7cdc3db18b992c9bd81df71e0
Sasha Ryabchun <sasharyabchun@gmail.com>
2018-06-28 01:07:46 +0300

    прикрутил EF

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
 
@@ -12,9 +14,21 @@ namespace SportsStore
 {
     public class Startup
     {
+        public static IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<IProductRepository, FakeProductRepository>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration["Data:SportsStoreProducts:ConnectionString"]));
+            services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddMvc();
         }
 
@@ -31,6 +45,7 @@ namespace SportsStore
                     template: "{controller=Product}/{action=List}/{id?}"
                     );
             });
+            //SeedData.EnsurePopulated(app);
         }
     }
 }
